f34c571787d3917574be6ead34856a196fae8dea
Byron-Selvage <bselvage@mines.edu>
2026-09-16 22:31:31 -0600

    Expose methods

--- comdas/codecs/__init__.py
+++ comdas/codecs/__init__.py
@@ -1,1 +1,6 @@
 """Compression codec interfaces and implementations."""
+
+from comdas.codecs.base import Codec, CompressedPayload
+from comdas.codecs.svd import SVDCodec, SVDPayload
+
+__all__ = ["Codec", "CompressedPayload", "SVDCodec", "SVDPayload"]
